4bf2e5318f306bbaa744a41c5c749c3990a8bffd
ikeda akira <oakirao18@gmail.com>
2019-01-14 17:24:53 +0900

    add

---
+++ catkin_ws/build/mypkg2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ikeda/robosys_ros/catkin_ws/src/mypkg2
+# Build directory: /home/ikeda/robosys_ros/catkin_ws/build/mypkg2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
